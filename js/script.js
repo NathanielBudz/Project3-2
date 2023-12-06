8dfc5602c2edbcaed8dda268c77b88b474128d3a
NathanielBudz <143546085+NathanielBudz@users.noreply.github.com>
2023-12-05 19:46:14 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-function init(){
-	var button = document.getElementById('entrybutton');
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("resize", function () {
+    applyResponsiveStyles();
+  });
 
-  function useralert(){
-    var textbox = document.getElementById('entryinput');
-    document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Nathaniel Budz:  " + textbox.value);
-  }
+  applyResponsiveStyles();
+});
 
-  button.addEventListener('click', useralert);
+function applyResponsiveStyles() {
+  if (window.innerWidth <= 600) {
+    document.body.style.fontSize = "14px";
+    document.body.style.lineHeight = "1.6";
+    document.querySelector("h1").style.fontSize = "24px";
+    document.querySelector("nav ul").style.textAlign = "left";
+    document.querySelector("nav ul").style.paddingLeft = "10px";
+  } else if (window.innerWidth <= 1024) {
+    document.body.style.fontSize = "16px";
+    document.body.style.lineHeight = "1.8";
+    document.querySelector("h1").style.fontSize = "28px";
+    document.querySelector("nav ul").style.textAlign = "center";
+    document.querySelector("nav ul").style.paddingLeft = "0";
+  } else {
+    document.body.style.fontSize = "18px";
+    document.body.style.lineHeight = "2";
+    document.querySelector("h1").style.fontSize = "32px";
+    document.querySelector("nav ul").style.textAlign = "center";
+    document.querySelector("nav ul").style.paddingLeft = "0";
   }
-  
-window.addEventListener('load', init);
+}
